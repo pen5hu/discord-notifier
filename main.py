39a25cffdb1bacb3143d433774e73e455479ec56
pen5hu <147407953+pen5hu@users.noreply.github.com>
2025-10-06 22:12:13 +0900

    🐛 Github actionで実行されるpythonに環境変数を渡す

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from dotenv import load_dotenv
 from infrastructure.discord_repository import DiscordRepository
@@ -8,7 +7,6 @@ from controller.notify_after_birth_days_controller import NotifyAfterBirthDaysCo
 
 def main():
     load_dotenv()
-    print(f"DEBUG: DISCORD_WEBHOOK_URL exists? {bool(os.getenv('DISCORD_WEBHOOK_URL'))}")
 
     # repository
     discord_repository = DiscordRepository()
